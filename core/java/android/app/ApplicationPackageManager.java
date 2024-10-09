a431db4b2cfd776db186db2cd52122d0eb4ed14a
songlongdaiduong <song.long.dai.duong@gmail.com>
2024-10-09 11:52:45 +0800

    core: Prevent an NPE of DevicePolicyManager object

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -1989,6 +1989,11 @@ public class ApplicationPackageManager extends PackageManager {
             return icon;
         }
 
+        DevicePolicyManager devicePolicyManager = getDevicePolicyManager();
+        if (devicePolicyManager == null) {
+            return icon;
+        }
+
         final Drawable badgeForeground = getDevicePolicyManager().getResources().getDrawable(
                 getUpdatableUserIconBadgeId(user),
                 SOLID_COLORED,
