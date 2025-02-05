c8c0e972ebf625c197cd6614281602eca28f754e
minaripenguin <minaripenguin@users.noreply.github.com>
2025-02-05 13:29:59 +0800

    services: Introduce Pocket Mode [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [Simplify keyguard callbacks]
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -289,6 +289,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
+import org.custom.server.PocketModeService;
+
 /**
  * WindowManagerPolicy implementation for the Android phone UI.  This
  * introduces a new method suffix, Lp, for an internal lock of the
@@ -335,6 +337,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     static final int LONG_PRESS_POWER_GO_TO_VOICE_ASSIST = 4;
     static final int LONG_PRESS_POWER_ASSISTANT = 5; // Settings.Secure.ASSISTANT
     static final int LONG_PRESS_POWER_TORCH = 6;
+    static final int LONG_PRESS_POWER_HIDE_POCKET_LOCK = 7;
 
     // must match: config_veryLongPresOnPowerBehavior in config.xml
     // The config value can be overridden using Settings.Global.POWER_BUTTON_VERY_LONG_PRESS
@@ -1734,7 +1737,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 msg.setAsynchronous(true);
                 msg.sendToTarget();
                 break;
+            case LONG_PRESS_POWER_HIDE_POCKET_LOCK:
+                mPowerKeyHandled = true;
+                getPocketModeInstance().disablePocketLock();
+                break;
         }
+    }
+
+    private PocketModeService getPocketModeInstance() {
+        return PocketModeService.getInstance(mContext);
     }
 
     private void powerVeryLongPress() {
@@ -1821,7 +1832,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mTorchLongPressPowerEnabled && (!isScreenOn() || isDozeMode() || mTorchEnabled)) {
             return LONG_PRESS_POWER_TORCH;
         }
-
+        if (getPocketModeInstance().isOverlayShowing()) {
+            return LONG_PRESS_POWER_HIDE_POCKET_LOCK;
+        }
         // If the config indicates the assistant behavior but the device isn't yet provisioned, show
         // global actions instead.
         if (mLongPressOnPowerBehavior == LONG_PRESS_POWER_ASSISTANT &&
@@ -2117,7 +2130,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void handleScreenShot(@WindowManager.ScreenshotSource int source,
             @WindowManager.ScreenshotType int type) {
-        mDefaultDisplayPolicy.takeScreenshot(type, source);
+        if (!getPocketModeInstance().isOverlayShowing()) {
+            mDefaultDisplayPolicy.takeScreenshot(type, source);
+        }
     }
 
     @Override
@@ -5892,6 +5907,23 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         final boolean interactive = (policyFlags & FLAG_INTERACTIVE) != 0;
+        
+        if (getPocketModeInstance().isOverlayShowing()) {
+            if (keyCode != KeyEvent.KEYCODE_POWER &&
+                keyCode != KeyEvent.KEYCODE_VOLUME_UP &&
+                keyCode != KeyEvent.KEYCODE_VOLUME_DOWN &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_PLAY &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_PAUSE &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE &&
+                keyCode != KeyEvent.KEYCODE_HEADSETHOOK &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_STOP &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_NEXT &&
+                keyCode != KeyEvent.KEYCODE_MEDIA_PREVIOUS &&
+                keyCode != KeyEvent.KEYCODE_VOLUME_MUTE) {
+                return 0;
+            }
+        }
+
         final boolean canceled = event.isCanceled();
         final int displayId = event.getDisplayId();
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
@@ -6933,6 +6965,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.onStartedGoingToSleep(pmSleepReason);
         }
+        getPocketModeInstance().onInteractiveChanged(false);
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -7004,6 +7037,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.onStartedWakingUp(pmWakeReason, mCameraGestureTriggered);
         }
+
+        getPocketModeInstance().setSystemReady();
+        
+        getPocketModeInstance().onInteractiveChanged(true);
 
         mCameraGestureTriggered = false;
     }
