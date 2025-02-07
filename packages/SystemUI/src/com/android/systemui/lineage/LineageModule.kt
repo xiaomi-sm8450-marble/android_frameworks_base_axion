f2455b435d4a2d2ca41a0e0e30f3273fbd59fa37
minaripenguin <minaripenguin@users.noreply.github.com>
2025-02-07 14:04:13 +0800

    SystemUI: Introduce Volume QS Tile
    Change-Id: I230b9eaee4706f4bd396bb06d2637c57ac4649d1
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -27,6 +27,7 @@ import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.VolumeControlTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
 
@@ -96,6 +97,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject VolumeControlTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VolumeControlTile.TILE_SPEC)
+    fun bindVolumeControlTile(volumeControlTile: VolumeControlTile): QSTileImpl<*>
 
     /** Inject VpnTile into tileMap in QSModule */
     @Binds
