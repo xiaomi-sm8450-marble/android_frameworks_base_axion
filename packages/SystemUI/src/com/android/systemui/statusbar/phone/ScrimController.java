82444279e3af691eefb11b18c470a47e12f8a016
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 18:16:38 +0800

    [AxionUI] QuickSettings UI changes
    Change-Id: Ic25175b661764ea5aa2f801651963a90ed70d587
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1557,7 +1557,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             state.setSurfaceColor(surface);
         }
 
-        mBehindColors.setMainColor(surface);
+        mBehindColors.setMainColor(background);
         mBehindColors.setSecondaryColor(accent);
         final boolean isSurfaceBackgroundLight = !ContrastColorUtil.isColorDark(surface);
         mBehindColors.setSupportsDarkText(isSurfaceBackgroundLight);
