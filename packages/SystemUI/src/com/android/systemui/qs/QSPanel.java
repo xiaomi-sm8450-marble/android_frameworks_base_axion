4de15181bbd389bb8284a47a362ec4d2226428ac
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-26 20:01:11 +0800

    [AxionUI] Brightness slider improvements
    Change-Id: If546aadb793097cfea6e2a3cd583ab897c363cbf
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -515,11 +515,6 @@ public class QSPanel extends LinearLayout {
             switchToParent(mFooter, parent, index);
             index++;
         }
-        
-        if (mBrightnessView != null) {
-            switchToParent(mBrightnessView, parent, index);
-            index++;
-        }
     }
 
     private void switchToParent(View child, ViewGroup parent, int index) {
